49fa821dd8eb7fe0a4c79ac57d2e17bf38afbf76
Vishalsai Daswani <vishalsai@gmail.com>
2014-04-21 00:42:28 -0400

    updated prototype client code

--- server_client/client.py
+++ server_client/client.py
@@ -2,12 +2,13 @@
 
 import socket, thread
 
-def read_stdin(s):
+def read_socket(s):
     while (True):
-        var = raw_input()
-        if (var == '0' or var == '1'):
-            s.sendall(var) 
-        return
+    data = s.recv(1024)
+        if (data == '0'):
+            photo_handler()
+        elif (data == '1'):
+            video_handler()
 
 def photo_handler():
     """ Function called when Pi receives a photo signal. CHANGE THIS """
@@ -23,13 +24,13 @@ PORT = 5000                     # The same port as used by the server
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
-count = 0
-thread.start_new_thread(read_stdin, (s,))
+thread.start_new_thread(read_socket, (s,))
+
 while (True):
-    #s.sendall('Hello, world')
-    data = s.recv(1024)
-    if (data == '0'):
-        photo_handler()
-    elif (data == '1'):
-        video_handler()
+    var = raw_input()
+    if (var == '0' or var == '1'):
+        s.sendall(var) 
+    return
+
+
 s.close()
